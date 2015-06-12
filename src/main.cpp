e392188881257783561870653e495e808bcb4641
Robin Chavignat <r.chavignat@gmail.com>
2015-06-12 20:52:12 +0200

    Switched project to CMake. Removed makefile. Added CMakeLists.txt. Various updates to let the project build. Oh my god, this code is really ugly. And I used to be proud of this monstrosity. Small update to the gitignore (should I remove this from the repo?)

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
-#include "Chip8.h"
-#include "Chip8_FileException.h"
 #include <boost/log/trivial.hpp>
 #include <SDL2/SDL.h>
+#include "Chip8.h"
+#include "Chip8_config.h"
+#include "Chip8_FileException.h"
 
 using namespace std;
 
@@ -11,18 +12,24 @@ using namespace std;
 // 1 : Error loading SDL
 // 2 : Error loading rom file
 int main(int argc, char* argv[]) {
-  Chip8* context;
-  try { context = new Chip8(argc, argv); }
-  catch(Chip8_Exception const& e) {
-    cout << "Error loading SDL" << endl;
-    return 1; }
-  catch(...) {
-    cout << "Unknown exception" << endl;
-    throw; }
+    cout << "Starting Chip8 v"  << Chip8_VERSION_MAJOR << '.' << Chip8_VERSION_MINOR << endl;
+    Chip8* context;
+    try {
+        context = new Chip8(argc, argv);
+    }
+    catch(Chip8_Exception const& e) {
+        cerr << "Error loading SDL" << endl;
+        return 1;
+    }
+    catch(...) {
+        cerr << "Unknown exception" << endl;
+        throw;
+    }
 
-  BOOST_LOG_TRIVIAL(info) << "Chip8 emu initialized successfully!";
+    BOOST_LOG_TRIVIAL(info) << "Chip8 emu initialized successfully!";
 
-  context->run();
+    context->run();
 
-  delete context;
-  return 0; }
+    delete context;
+    return 0;
+}
